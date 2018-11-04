2311d17df93fd8e8f038fc6290759cfd46c32322
Arnaud Didry <arnaud@fizix.io>
2018-11-04 15:41:55 +0100

    Add support for HtmlWebpackPlugin v4.0.0

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 // html-webpack-inject-attributes-plugin
 var assign = require('lodash.assign');
 var forEach = require('lodash.foreach');
+var HtmlWebpackPlugin;
+
+try {
+  // eslint-disable-next-line global-require
+  HtmlWebpackPlugin = require('html-webpack-plugin');
+} catch (e) {
+  if (!(e instanceof Error) || e.code !== 'MODULE_NOT_FOUND') {
+    throw e;
+  }
+}
+
+const PLUGIN_NAME = "htmlWebpackInjectAttributesPlugin";
 
 function addAttr(compilation, tags, key, val) {
     if (!tags || !tags.length) {
@@ -19,8 +31,15 @@ function addAttr(compilation, tags, key, val) {
 function alterAssetTags(compilation, htmlPluginData, callback) {
     var options = assign({}, this.options, htmlPluginData.plugin.options && htmlPluginData.plugin.options.attributes);
     forEach(options, function (val, key) {
-        addAttr(compilation, htmlPluginData.head, key, val);
-        addAttr(compilation, htmlPluginData.body, key, val);
+        if (htmlPluginData.assetTags) {
+            // HtmlWebpackPlugin 4+
+            addAttr(compilation, htmlPluginData.assetTags.scripts, key, val);
+            addAttr(compilation, htmlPluginData.assetTags.styles, key, val);
+        } else {
+            // HtmlWebpackPlugin 3
+            addAttr(compilation, htmlPluginData.head, key, val);
+            addAttr(compilation, htmlPluginData.body, key, val);
+        }
     });
 
     if (typeof callback === 'function') {
@@ -38,12 +57,23 @@ htmlWebpackInjectAttributesPlugin.prototype.apply = function (compiler) {
     var self = this;
 
     if (compiler.hooks) {
-        compiler.hooks.compilation.tap("htmlWebpackInjectAttributesPlugin", function (compilation) {
-            compilation
-                .hooks
-                .htmlWebpackPluginAlterAssetTags
-                .tap("htmlWebpackInjectAttributesPlugin", alterAssetTags.bind(self, compilation));
-        });
+        if (HtmlWebpackPlugin && HtmlWebpackPlugin.getHooks) {
+            // HtmlWebpackPlugin 4+
+            compiler.hooks.compilation.tap(PLUGIN_NAME, function (compilation) {
+                HtmlWebpackPlugin
+                    .getHooks(compilation)
+                    .alterAssetTags
+                    .tapAsync(PLUGIN_NAME, alterAssetTags.bind(self, compilation));
+            });
+        } else {
+            // HtmlWebpackPlugin 3
+            compiler.hooks.compilation.tap(PLUGIN_NAME, function (compilation) {
+                compilation
+                    .hooks
+                    .htmlWebpackPluginAlterAssetTags
+                    .tap(PLUGIN_NAME, alterAssetTags.bind(self, compilation));
+            });
+        }
     } else {
         compiler.plugin('compilation', function (compilation) {
             compilation.plugin('html-webpack-plugin-alter-asset-tags', alterAssetTags.bind(self, compilation));
